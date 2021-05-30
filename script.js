d071a54f50c218d168b33de19cb73e68996c4c6f
Prateek <prateeksingh8841@gmail.com>
2021-05-30 13:57:59 -0700

    added_filters_and_zoom

--- script.js
+++ script.js
@@ -6,6 +6,43 @@ let chunks = [];
 let mediaRecorder;
 let isRecording = false;
 
+// variables for handling filter change
+let body = document.querySelector('body');
+let filters = document.querySelectorAll('.filter');
+let filter = ''; // what filter is selected
+
+// variables for handling zoom
+let minZoom = 1;
+let maxZoom = 3;
+let currZoom = 1;
+let zoomIn = document.querySelector('.zoom-in');
+let zoomOut = document.querySelector('.zoom-out');
+
+for(let i = 0; i < filters.length; i++)
+{
+    filters[i].addEventListener('click', function(e) {
+        filter = e.currentTarget.style.backgroundColor;
+        removeFilter();
+        applyFilter(filter);
+    });
+}
+
+zoomIn.addEventListener('click', function() {
+    if(currZoom < maxZoom)
+    {
+        currZoom += 0.1;
+        video.style.transform = `scale(${currZoom})`;
+    }
+});
+
+zoomOut.addEventListener('click', function() {
+    if(currZoom > minZoom)
+    {
+        currZoom -= 0.1;
+        video.style.transform = `scale(${currZoom})`;
+    }
+});
+
 captureBtn.addEventListener('click', function() {
     capture();
 
@@ -29,6 +66,8 @@ recordBtn.addEventListener('click', function() {
     {
         mediaRecorder.start();
         isRecording = true;
+        currZoom = 1;
+        video.style.transform = `scale(${currZoom})`;
         document.querySelector('.record-bubble').classList.add('fader-animation');
     }
 });
@@ -64,6 +103,12 @@ function capture() { // click picture
     c.width = video.videoWidth;
     c.height = video.videoHeight;
     let ctx = c.getContext('2d');
+
+    // for scaling canvas, we need to shift origin and then back
+    ctx.translate(c.width / 2, c.height / 2);
+    ctx.scale(currZoom, currZoom);
+    ctx.translate(-c.width / 2, -c.height / 2);
+
     ctx.drawImage(video, 0, 0); // draw that video frame
 
     // save trick
@@ -72,4 +117,16 @@ function capture() { // click picture
     a.href = c.toDataURL();
     a.click();
     a.remove();
+}
+
+function applyFilter(filterColor) {
+    let filterDiv = document.createElement('div');
+    filterDiv.classList.add('filter-div');
+    filterDiv.style.backgroundColor = filterColor;
+    body.appendChild(filterDiv);
+}
+
+function removeFilter() {
+    let filterDiv = document.querySelector('.filter-div');
+    if(filterDiv) filterDiv.remove(); // if it is overlayed then remove the prev
 }
